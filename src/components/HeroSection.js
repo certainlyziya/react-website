522976d4283bd20e1294c1ac6748d46fec9b314c
ZIYA <80207627+certainlyziya@users.noreply.github.com>
2021-03-12 20:56:21 +0530

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,12 +2,10 @@ import React from 'react';
 import '../App.css';
 import { Button } from './Button';
 import './HeroSection.css';
-import video from '../videos/video-1.mp4';
 
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src={video} autoPlay loop muted />
       <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for?</p>
       <div className='hero-btns'>
